c6cf56d8819879de090d3ad91bcf8b7b06ae4c4b
ProudOne7 <peternyen7@gmail.com>
2024-03-09 15:53:32 -0600

    Edited imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import { useState } from 'react'
+import React from 'react'
+import { Route, Switch } from 'react-router-dom';
+import homePage from './pages/homePage.jsx'
+import searchPage from './pages/searchPage.jsx'
 
 function App() {
   return (
