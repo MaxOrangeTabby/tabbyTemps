84cdd60f50700dd3fbba9011dc85595540984516
ProudOne7 <peternyen7@gmail.com>
2024-03-10 22:39:22 -0500

    Fixed Imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom';
+import { Route } from "react-router-dom";
 import homePage from './pages/homePage.jsx'
 import searchPage from './pages/searchPage.jsx'
 
 function App() {
   return (
-    <div>hi</div>
+    <div>hjkhkjhi</div>
   )
 }
 
