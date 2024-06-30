54be39b48555d08ac811477ecd3558d9c135c6ea
ProudOne7 <peternyen7@gmail.com>
2024-06-30 16:32:35 -0500

    Added Routes

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,6 +1,18 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import LoadPage from './Components/LoadPage/LoadPage'
+import WeatherPage from './Components/WeatherPage/WeatherPage'
+import { PressStart } from './Components/Matter/MatterStepTwo'
+
 function App() {
   return (
-    <></>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LoadPage/>}></Route>
+        <Route path='/weather-page' element={<WeatherPage/>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
