9d37a25272b296859f240b0fd277548561b6d06e
ProudOne7 <peternyen7@gmail.com>
2024-07-02 18:26:58 -0500

    Fixed Route Elements

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
-import LoadPage from './Components/LoadPage/LoadPage'
-import WeatherPage from './Components/WeatherPage/WeatherPage'
-import { PressStart } from './Components/Matter/MatterStepTwo'
+import LoadPage from './Components/StartPage/StartPage'
+import SearchPage from './Components/SearchPage/SearchPage'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<LoadPage/>}></Route>
-        <Route path='/weather-page' element={<WeatherPage/>}></Route>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LoadPage/>}></Route>
+          <Route path='/weather-page' element={<SearchPage/>}></Route>
+        </Routes>
+      </BrowserRouter>
   )
 }
 
