e21b68366cc3cc74bbfcd189bf42abed3b4dfb50
ProudOne7 <peternyen7@gmail.com>
2024-07-03 16:27:50 -0500

    Removed Unused imports

--- Client/src/Components/StartPage/StartPage.tsx
+++ Client/src/Components/StartPage/StartPage.tsx
@@ -1,17 +1,15 @@
 import { useState } from 'react'
 import styles from './StartPage.module.css'
 
-import { animate, motion } from "framer-motion"
-import { useStepContext } from '@chakra-ui/react'
+import { motion } from "framer-motion"
 import { Link } from 'react-router-dom'
 
 
 function StartPage() {
   const [titleClicked, setTitleClicked] = useState(false)
-  const [btnFall, setBtnFall] = useState(false)
 
   const titleClickedFunc = () => {
-    setTitleClicked(!titleClicked)  
+    setTitleClicked(true)  
   }
 
   return (
