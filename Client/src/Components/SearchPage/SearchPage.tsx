851f48d2f0c8776cbc007f20a0a512f169019886
ProudOne7 <peternyen7@gmail.com>
2024-07-06 18:11:09 -0500

    Formatted Inline Styles

--- Client/src/Components/SearchPage/SearchPage.tsx
+++ Client/src/Components/SearchPage/SearchPage.tsx
@@ -13,6 +13,7 @@ import WeatherHighlights from '../WeatherHighlights/WeatherHighlights'
 import WeatherCard from '../WeatherCard/WeatherCard'
 import OrangeCatIcon from '../Images/orangeCatIcon.png'
 import OrangeTextBubble from '../Images/orangeTextBubble.png'
+import SearchPageBg from '../Images/searchPageBg.svg'
 
 
 function SearchPage() {
@@ -22,7 +23,7 @@ function SearchPage() {
   const [addDataObj, setAddDataObj] = useState<AddWeatherData | null>(null);
 
   const [city, setCity] = useState('');
-  const [btnClicked, setBtnClicked] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   
   const submitForm = async (e : React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -67,22 +68,17 @@ function SearchPage() {
         windKPH: res.data.current.wind_kph,
       });
       setDataLoadded(true)
+      setIsLoading(false)
+      console.log('Data Loaded')
     }
     catch (error){
       console.log(error)
     }
   };
 
-  const btnHover = () => {
-      setBtnClicked(true);
-  };
-
-  const btnHoverEnd = () => {
-    setBtnClicked(false);
-  }
 
   return (
-    <Flex bgColor={'white'} h={'100vh'} direction={'column'} align={'center'} justify={'space-evenly'} >
+    <Flex bgImg={SearchPageBg} h={'100vh'} direction={'column'} align={'center'} justify={'space-evenly'} >
       <Flex h={'20%'} w={'100%'} direction={'column'} align={'center'}>
         <WeatherHighlights />
       </Flex>
@@ -99,7 +95,7 @@ function SearchPage() {
         <motion.div initial={{opacity: 0}} animate={{opacity: [0,.25,.5,1]}} exit={{opacity: 0}}>
           <Flex>
             <Image boxSize='170px' src={OrangeTextBubble} alt={'Orange Cat Text Bubble'} mb={'4rem'}/>
-            <Text w={'50%'} pos={'absolute'} top={'0'} left={'0'} right={'0'} pt={'1.95rem'} pl={'1.25rem'} fontFamily={'Pixelify Sans'} whiteSpace={'break-spaces'}>Press The Square To Get More Details!</Text>
+            <Text w={'50%'} pos={'absolute'} top={'0'} left={'0'} right={'0'} pt={'1.95rem'} pl={'1.25rem'} fontFamily={'Pixelify Sans'} color={'#BF4E30'}>Press The Square To Get More Details!</Text>
           </Flex>
         </motion.div>
         }
@@ -132,12 +128,40 @@ function SearchPage() {
       <Spacer />
         <form onSubmit={submitForm}>
           <Flex direction={'column'} justify={'center'} align={'center'}>
-            <Input type='text' name='city' onChange={(event) =>setCity(event.target.value)}></Input>
-            <motion.div>
-              <Button as={motion.button} whileHover={{scale: 1.1, borderRadius: '30%'}} onHoverStart={btnHover} onHoverEnd={btnHoverEnd} type='submit' mt={'.5rem'}>
-                {!btnClicked ? 'Check The Weather' : <FontAwesomeIcon icon={faCat} />}
-              </Button>
-            </motion.div>
+            <Input 
+              type='text' 
+              name='city' 
+              onChange={(event) =>setCity(event.target.value)} 
+
+              variant='flushed'
+              borderBottom={'4px solid #BF4E30'}
+              bgColor={'transparent'}
+              color={'#BF4E30'}
+              
+              textAlign={'center'}
+              fontSize={['1.25rem','lg', 'lg', 'xl', '2xl']}
+              fontWeight={600}
+              letterSpacing={'.05rem'}
+            >
+            </Input>
+            <Button
+              type='submit'
+
+              onClick={() => setIsLoading(true)}
+
+              variant='outline'
+              color={'#BF4E30'}
+              mt={'.5rem'}
+              border={'none'}
+
+              fontWeight='800'
+              fontSize={['1.25rem','lg', 'lg', 'xl', '2xl']}
+
+              isLoading={isLoading}
+              spinner={<FontAwesomeIcon icon={faCat} />}
+            >
+              Check The Weather
+            </Button>
           </Flex>  
         </form>
       <Spacer />
