b29fd67fd94f17d5826d5adb0f37b3b1c09832b6
ProudOne7 <peternyen7@gmail.com>
2024-07-06 13:53:33 -0500

    Added Info Hint & Fixed Typo

--- Client/src/Components/SearchPage/SearchPage.tsx
+++ Client/src/Components/SearchPage/SearchPage.tsx
@@ -3,18 +3,21 @@ import {useState}  from 'react'
 
 import axios from 'axios'
 
-import { Flex, Spacer, Divider,  Input, Button } from '@chakra-ui/react'
-import { motion } from "framer-motion"
+import { Flex, Spacer, Divider,  Input, Button, Image, Text } from '@chakra-ui/react'
+import { AnimatePresence, motion } from "framer-motion"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCat } from '@fortawesome/free-solid-svg-icons'
 
 import { AddWeatherData, WeatherData} from '../Types/Types'
 import WeatherHighlights from '../WeatherHighlights/WeatherHighlights'
 import WeatherCard from '../WeatherCard/WeatherCard'
+import OrangeCatIcon from '../Images/orangeCatIcon.png'
+import OrangeTextBubble from '../Images/orangeTextBubble.png'
 
 
 function SearchPage() {
   const [dataLoaded, setDataLoadded] = useState(false);
+  const [catClicked, setCatClicked] = useState(false);
   const [dataObj, setDataObj] = useState<WeatherData | null>(null);
   const [addDataObj, setAddDataObj] = useState<AddWeatherData | null>(null);
 
@@ -61,7 +64,7 @@ function SearchPage() {
 
         windDir: res.data.current.wind_dir,
         windMPH: res.data.current.wind_mph,
-        windKPH: res.data.current.wind_keph,
+        windKPH: res.data.current.wind_kph,
       });
       setDataLoadded(true)
     }
@@ -79,29 +82,53 @@ function SearchPage() {
   }
 
   return (
-    
-    <Flex h={'100vh'} direction={'column'} align={'center'} justify={'space-evenly'}>
+    <Flex bgColor={'white'} h={'100vh'} direction={'column'} align={'center'} justify={'space-evenly'} >
       <Flex h={'20%'} w={'100%'} direction={'column'} align={'center'}>
         <WeatherHighlights />
       </Flex>
       <Divider orientation='horizontal' color={'black'}/>
-        {!dataLoaded ?
-        null
+
+      {!dataLoaded ? null
+      :
+      
+        <Flex direction={'row'} position={'absolute'} right={0} top={'40%'} overflow={'hidden'}>
+
+        <AnimatePresence>
+        {catClicked ? null
         :
-        <motion.div drag  
-        dragConstraints={{
-          top: -50,
-          left: -50,
-          right: 50,
-          bottom: 50,
-        }}
-        initial={{ opacity: 0, scale: 0.5 }} 
-        animate={{ opacity: 1, scale: [.85,1,1.05,1], }}
-        > 
-          <WeatherCard data={dataObj}  addData={addDataObj} />  
+        <motion.div initial={{opacity: 0}} animate={{opacity: [0,.25,.5,1]}} exit={{opacity: 0}}>
+          <Flex>
+            <Image boxSize='170px' src={OrangeTextBubble} alt={'Orange Cat Text Bubble'} mb={'4rem'}/>
+            <Text w={'50%'} pos={'absolute'} top={'0'} left={'0'} right={'0'} pt={'1.95rem'} pl={'1.25rem'} fontFamily={'Pixelify Sans'} whiteSpace={'break-spaces'}>Press The Square To Get More Details!</Text>
+          </Flex>
         </motion.div>
         }
-     
+        </AnimatePresence>
+
+
+        <motion.div initial={{x: '100%', rotate: 0}} animate={{x: ['50%', '0%']}}>
+          <Image right={0} objectFit={'contain'} boxSize='128px' src={OrangeCatIcon} alt={'Orange Cat Icon'} mt={'4rem'} onClick={() => setCatClicked(!catClicked)}/>
+        </motion.div>
+        </Flex>  
+      }
+
+      {!dataLoaded ?
+      null
+      :
+      <motion.div drag  
+      dragConstraints={{
+        top: -50,
+        left: -50,
+        right: 50,
+        bottom: 50,
+      }}
+      initial={{ opacity: 0, scale: 0.5 }} 
+      animate={{ opacity: 1, scale: [.85,1,1.05,1], }}
+      > 
+        <WeatherCard data={dataObj}  addData={addDataObj} />  
+      </motion.div>
+      }
+    
       <Spacer />
         <form onSubmit={submitForm}>
           <Flex direction={'column'} justify={'center'} align={'center'}>
